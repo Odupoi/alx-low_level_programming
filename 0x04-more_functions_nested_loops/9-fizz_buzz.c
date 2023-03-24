c749991e33e88c560beafafb2d82bbc12d805825
Odupoi <rananicholad8@gmail.com>
2023-03-24 13:46:32 -0700

    Fizz-Buzz printer

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - for printing the fizzbuzz
+ * @i: parameter
+ * Return: 0 if a success
+ */
+
+int main(void)
+{
+	int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("\n");
+return (0);
+}
